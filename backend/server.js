f7be90a6a924407e63aa077c58913c69fe844cb3
tibiteo12 <teodosiutiberiu@yahoo.com>
2022-06-12 17:18:09 +0300

    node server

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express, { application } from "express";
+import data from "./data.js";
+
+const app = express();
+
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`);
+});
