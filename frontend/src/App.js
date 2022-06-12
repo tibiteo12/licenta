dce68ecb38d844239c12d5945f481d79581a904b
tibiteo12 <teodosiutiberiu@yahoo.com>
2022-06-12 15:37:43 +0300

    routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,23 @@
-import data from "./data";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href='/'>GetFit</a>
-      </header>
-      <main>
-        <h1>Produse populare</h1>
-        <div className='products'>
-          {data.products.map((product) => (
-            <div className='product' key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className='product-info'>
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>{product.price} RON</strong>
-                </p>
-                <button>Adaugă în coș!</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to='/'>GetFit</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path='/product/:slug' element={<ProductScreen />} />
+            <Route path='/' element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
