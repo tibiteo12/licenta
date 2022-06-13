5961d5341bb73918a04ea017d8b7553587249628
tibiteo12 <teodosiutiberiu@yahoo.com>
2022-06-13 13:45:27 +0300

    loading

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.get("/api/products/slug/:slug", (req, res) => {
   if (product) {
     res.send(product);
   } else {
-    res.status(404).send({ message: "Product not found" });
+    res.status(404).send({ message: "Produsul nu a fost găsit!" });
   }
   res.send(data.products);
 });
